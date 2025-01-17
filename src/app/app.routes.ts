012ff350c33524dfaaa7f308e8d4749f56328878
Larissa Oliveira <oliveir5uwm@gmail.com>
2025-01-17 20:03:10 -0300

    feat: add routing for new page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'products',
+    loadComponent: () => import('./components/product-list/product-list.component').then(m => m.ProductListComponent),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+];
